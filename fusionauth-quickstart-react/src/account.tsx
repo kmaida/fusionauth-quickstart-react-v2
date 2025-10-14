7b0de1130de173ba94673430380d253fbe394994
Kim Maida <1884525+kmaida@users.noreply.github.com>
2025-10-14 10:06:02 -0400

    Initial commit - new react qs (Kim)

---
+++ fusionauth-quickstart-react/src/account.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useFusionAuth } from '@fusionauth/react-sdk';
+
+export default function Account() {
+  const navigate = useNavigate();
+  const { isLoggedIn, isFetchingUserInfo, startLogout, userInfo } = useFusionAuth();
+  const [newUserInfo, setNewUserInfo] = useState({'given_name': '', 'family_name': '', 'birthdate': ''});
+  
+  useEffect(() => { if (!isLoggedIn) navigate("/"); }, [isLoggedIn, navigate]);
+  
+  async function getUserInfo() {
+    const response = await fetch('http://localhost:9011/app/me', {
+      'method': 'GET',
+      'credentials': 'include',
+      'headers': { 'Accept': 'application/json' }
+    });
+    const info = await response.json();
+    setNewUserInfo(info);
+  }
+
+  if (!isLoggedIn || isFetchingUserInfo) return null;
+  
+  return (
+    <div>
+      <div className="titlebar">
+        <span className='white'>{userInfo?.email}</span>
+        <button className='button' onClick={() => startLogout()}>Logout</button>
+      </div>
+      <div className='centerContainer'>
+        <div className="userInfoGrid">
+          <div>Name:</div>
+          <div>{newUserInfo?.given_name} {newUserInfo?.family_name}</div>
+          <div>Birthdate:</div>
+          <div>{newUserInfo?.birthdate}</div>
+        </div>
+        <br />
+        <div>
+          <button className="button" onClick={getUserInfo}>Show your info</button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ fusionauth-quickstart-react/src/home.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useFusionAuth} from '@fusionauth/react-sdk';
+
+export default function Home() {
+  const navigate = useNavigate();
+  const { isLoggedIn, startLogin, startLogout, userInfo } = useFusionAuth();
+  useEffect(() => {
+    if (isLoggedIn && sessionStorage.getItem('justLoggedIn') === 'true') {
+      sessionStorage.removeItem('justLoggedIn');
+      navigate("/account");
+    }
+  }, [isLoggedIn, navigate]);
+
+  return (
+    <div>
+      <div className="titlebar">
+        {isLoggedIn ? (
+          <>
+          <span className='white'>{userInfo?.email}</span>
+          <button className='button' onClick={() => startLogout()}>Logout</button>
+          </>
+        ) : (
+          <button
+            className='button'
+            onClick={() => {
+              sessionStorage.setItem('justLoggedIn', 'true');
+              startLogin("state-from-login");
+            }}
+          >
+            Login
+          </button>
+        )}
+      </div>
+      <div className='centerContainer'>
+        <div>Log in to request your information</div>
+      </div>
+    </div>
+  );
+};
